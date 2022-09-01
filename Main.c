212de7a7d6c0674fdf77a3985b93438abc7996ed
bmilioli <brunomiliolif@gmail.com>
2022-08-31 23:32:11 -0300

    Inicio

--- Main.c
+++ Main.c
@@ -10,9 +10,9 @@ typedef struct
 
 void coletaDados(CHUVEIRO *chuveiro)
 {
-    printf("Informe a altura do teto:");
+    printf("Informe a altura do teto: ");
     scanf("%f", &chuveiro->teto);
-    printf("Informe a altura do armazenamento:");
+    printf("Informe a altura do armazenamento: ");
     scanf("%f", &chuveiro->altura);
 }
 
@@ -46,12 +46,59 @@ void tabela5(CHUVEIRO *chuveiro)
         chuveiro->tempo = 1;
         chuveiro->nChuveiros = 12;
     }
+
+    else if (chuveiro->altura <= 7.6 && chuveiro->teto <= 9.1)
+    {
+        do
+        {
+            printf("Escolha do fator K \n");
+            printf("Opções: 200, 240, 320 e 360 \n");
+            scanf("%d", &chuveiro->fatorK);
+        } while (chuveiro->fatorK != 200 && chuveiro->fatorK != 240 && chuveiro->fatorK != 320 && chuveiro->fatorK != 360);
+        switch (chuveiro->fatorK)
+        {
+        case 200:
+            chuveiro->pressao = 3.4;
+            break;
+        case 240:
+            chuveiro->pressao = 2.4;
+            break;
+        case 320:
+            chuveiro->pressao = 1.7;
+            break;
+        case 360:
+            chuveiro->pressao = 1.0;
+            break;
+        }
+        chuveiro->tempo = 1;
+        chuveiro->nChuveiros = 12;
+    }
+    else if (chuveiro->altura <= 7.6 && chuveiro->teto <= 9.8)
+    {
+        do
+        {
+            printf("Escolha do fator K \n");
+            printf("Opções: 200, 240, 320 e 360 \n");
+            scanf("%d", &chuveiro->fatorK);
+        } while (chuveiro->fatorK != 200 && chuveiro->fatorK != 240);
+        switch (chuveiro->fatorK)
+        {
+        case 200:
+            chuveiro->pressao = 4.1;
+            break;
+        case 240:
+            chuveiro->pressao = 2.9;
+            break;
+        }
+        chuveiro->tempo = 1;
+        chuveiro->nChuveiros = 12;
+    }
 }
 
 int main()
 {
     CHUVEIRO chuveiro;
-    setlocale(LC_ALL, "Portuguese");
+    // setlocale(LC_ALL, "Portuguese");
 
     coletaDados(&chuveiro);
     tabela5(&chuveiro);
